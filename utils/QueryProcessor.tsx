9099cc5c948dffe88c44e4ac63af46046217d7e5
Luciana <lrequena@andrew.cmu.edu>
2025-10-06 14:32:12 -0400

    Added AndrewID question in Query Processor

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -8,7 +8,11 @@ export default function QueryProcessor(query: string): string {
   }
 
   if (query.toLowerCase().includes("name")) {
-    return "Rohan";
+    return "Luciana";
+  }
+  
+  if (query.toLowerCase().includes("andrew id")) {
+    return "Lrequena";
   }
 
   return "";
